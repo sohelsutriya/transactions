fd8dfed712b2fec45634333be8d959a35fdaa85c
sohel_sutriya <sohelsutriya143@gmail.com>
2020-05-05 21:20:56 +0530

    updated

--- src/main/java/com/cg/obs/TransasctionsApplication.java
+++ src/main/java/com/cg/obs/TransasctionsApplication.java
@@ -2,12 +2,19 @@ package com.cg.obs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TransasctionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransasctionsApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate createTemplate() {
+		return new RestTemplate();
+	}
 }
